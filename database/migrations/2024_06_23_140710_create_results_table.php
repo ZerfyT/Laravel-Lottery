6f9dca48257824cf5c69727035da769cde7bc28f
NipunLiyanage69 <nipun.rightmo@gmail.com>
2024-06-23 19:52:43 +0530

    added migrations

---
+++ database/migrations/2024_06_23_140710_create_results_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Lottery;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(Lottery::class);
+            $table->string('data');
+            $table->date('date');
+            $table->integer('round');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Lottery;
+use App\Models\Result;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +20,27 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $lottery = Lottery::create([
+            'name' => 'Test Lottery',
+            'description' => 'Test Lottery Description',
+            'image' => 'https://via.placeholder.com/150',
+            'pattern' => '/^\d{2}\s\d{2}\s\d{2}\s\d{2}$/'
+        ]);
+
+        $result = Result::create([
+            'lottery_id' => $lottery->id,
+            'data' => '11 25 76 23',
+            'date' => '2022-01-01',
+            'round' => 1
+        ]);
+
+        $result2 = Result::create([
+            'lottery_id' => $lottery->id,
+            'data' => '78 43 28 92',
+            'date' => '2022-01-02',
+            'round' => 2
         ]);
     }
 }
